5d3821a239962d6563f7c797f84657a5692def49
Andres Revolledo Galvez <aqwssama@gmail.com>
2015-06-07 22:17:02 -0500

    Base Proyect

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 game = new Phaser.Game(480, 320, Phaser.AUTO);
-game.state.add('Boot', Boost);
+game.state.add('Boost', Boost);
 game.state.add('Preload', Preload);
 game.state.add('Game', Game);
-game.state.start('Boot');
+game.state.start('Boost');
